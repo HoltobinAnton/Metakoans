2fb2f0c143a4521b0ac7ab7a5d09c34c0ec19974
YogurtYoung <antoshkabest@ua.fm>
2016-10-31 05:48:59 +0200

    metakoans

---
+++ knowledge.rb
@@ -0,0 +1,31 @@
+class Module
+def attribute(attr, &block)
+  case attr
+    when Hash
+      name    = attr.keys[0]
+      value = attr[name]
+    when String, Symbol
+      name    = attr
+      value = nil
+  end
+
+  sym = name.to_sym
+  instance_variable = :"@#{name}"
+
+  define_method(:"#{name}?") do
+    send(sym)
+  end
+
+  define_method(:"#{name}=") do |value|
+    instance_variable_set(:"@#{name}", value)
+  end
+
+  define_method(sym) do
+    if instance_variable_defined?(instance_variable)
+      instance_variable_get(instance_variable)
+    else
+      block ? instance_eval(&block) : value
+    end
+  end
+end
+end
